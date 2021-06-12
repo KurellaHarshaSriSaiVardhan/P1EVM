da1b152a3d278cbce030c7fbc06ee6729a8937a7
KurellaHarshaSriSaiVardhan <82654939+KurellaHarshaSriSaiVardhan@users.noreply.github.com>
2021-06-12 07:44:40 +0530

    p1evm

---
+++ EVM/evm.js
@@ -0,0 +1,328 @@
+var h;
+function ha()
+{
+	document.getElementById('qa').src="bb.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qa').style.background="blue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+	document.getElementById('tex').src="cong.png";
+	document.getElementById('dull').style="visibility:visible;"
+	h=0;
+}
+function hb()
+{
+	document.getElementById('qb').src="bb.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qb').style.background="blue";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+	document.getElementById('tex').src="bj.jpg";
+	document.getElementById('dull').style="visibility:visible;"
+	h=1;
+}
+function hc()
+{
+	document.getElementById('qc').src="bb.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qc').style.background="blue";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+	document.getElementById('tex').src="tdp.png";
+	document.getElementById('dull').style="visibility:visible;"
+	h=2;
+}
+function hd()
+{
+	document.getElementById('qd').src="bb.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qd').style.background="blue";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+	document.getElementById('dull').style="visibility:visible;"
+	document.getElementById('tex').src="f.png";
+	h=3;
+}
+function he()
+{
+	document.getElementById('qe').src="bb.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qe').style.background="blue";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+	document.getElementById('tex').src="ls.png";
+	document.getElementById('dull').style="visibility:visible;"
+	h=4;
+}
+function hf()
+{
+	document.getElementById('qf').src="bb.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qf').style.background="blue";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+	document.getElementById('tex').src="trs.jpg";
+	document.getElementById('dull').style="visibility:visible;"
+	h=5;
+}
+function hg()
+{
+	document.getElementById('qg').src="bb.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qg').style.background="blue";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+	document.getElementById('tex').src="pk.png";
+	document.getElementById('dull').style="visibility:visible;"
+	h=6;
+}
+function hh()
+{
+	document.getElementById('qh').src="bb.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').style.background="blue";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('tex').src="nt.jpg";
+	document.getElementById('dull').style="visibility:visible;"
+	h=7;
+}
+function ias()
+{
+	document.getElementById('qh').style="visibility:visible;"
+}
+function on()
+{
+	document.getElementById('onn').src="off.png";
+    document.getElementById('pow').src="21.png";
+	document.getElementById('tab').style="opacity:9;"
+	document.getElementById('dull').style="opacity:9;"
+	document.getElementById('qa').style="visibility:visible;"
+	document.getElementById('qb').style="visibility:visible;"
+	document.getElementById('qc').style="visibility:visible;"
+	document.getElementById('qd').style="visibility:visible;"
+	document.getElementById('qe').style="visibility:visible;"
+	document.getElementById('qf').style="visibility:visible;"
+	document.getElementById('qg').style="visibility:visible;"
+	document.getElementById('qh').style="visibility:visible;"
+	
+
+}
+function of()
+{
+	document.getElementById('tex').src="ec.png";
+	document.getElementById('onn').src="on.png";
+    document.getElementById('pow').src="20.png";
+	document.getElementById('tab').style="opacity:0.3;"
+	document.getElementById('dull').style="opacity:0.3;"
+	document.getElementById('qa').style="visibility:hidden;"
+	document.getElementById('qe').style="visibility:hidden;"
+	document.getElementById('qf').style="visibility:hidden;"
+	document.getElementById('qg').style="visibility:hidden;"
+	document.getElementById('qb').style="visibility:hidden;"
+	document.getElementById('qc').style="visibility:hidden;"
+	document.getElementById('qd').style="visibility:hidden;"
+	document.getElementById('qh').style="visibility:hidden;"
+	document.getElementById('dull').style="visibility:hidden;"
+}
+function cancel()
+{
+	document.getElementById('tex').src="ec.png";
+	document.getElementById('qa').src="b.png";
+	document.getElementById('qb').src="b.png";
+	document.getElementById('qc').src="b.png";
+	document.getElementById('qd').src="b.png";
+	document.getElementById('qe').src="b.png";
+	document.getElementById('qf').src="b.png";
+	document.getElementById('qg').src="b.png";
+	document.getElementById('qh').src="b.png";
+	document.getElementById('qa').style.background="skyblue";
+	document.getElementById('qb').style.background="skyblue";
+	document.getElementById('qc').style.background="skyblue";
+	document.getElementById('qd').style.background="skyblue";
+	document.getElementById('qe').style.background="skyblue";
+	document.getElementById('qf').style.background="skyblue";
+	document.getElementById('qg').style.background="skyblue";
+	document.getElementById('qh').style.background="skyblue";
+
+
+
+}
+var i=0;
+function rar()
+{
+	/*var sq=document.getElementById('tex').src="cong.png";
+	var sw=document.getElementById('tex').src="bj.jpg";
+	var se=document.getElementById('tex').src="tdp.png";
+	var sr=document.getElementById('tex').src="f.png";
+	var st=document.getElementById('tex').src="ls.png";
+	var sy=document.getElementById('tex').src="trs.jpg";
+	var su=document.getElementById('tex').src="pk.png";
+	var so=document.getElementById('tex').src="nt.jpg";*/
+   var ps=['cong.png','bj.jpg','tdp.png','f.png','ls.png','trs.jpg','pk.png','nt.jpg'];
+     document.getElementById("tex").src=ps[i];
+	 i=i+1;
+	 if(i==8)
+	 	i=0;
+}
+var j=0;
+function lar()
+{
+	var pls=['nt.jpg','pk.png','trs.jpg','ls.png','f.png','tdp.png','bj.jpg','cong.png'];
+     document.getElementById("tex").src=pls[j];
+	 j=j+1;
+	 if(j==8)
+	 	j=0;
+}
+var id=[];
+var a=0;
+function start()
+{   
+	
+	var ea=prompt(" Please enter ur Voter ID");
+	var b=0;
+	id[a]=ea;
+		a++;
+		for(k=0;k<a;k++)
+		{
+			if(ea==id[k])
+			{
+				b++;
+			}
+		}
+		if(b>1)
+		{
+			alert(ea+"have already voted");
+			start();
+		}
+
+}
+var gh=[0,0,0,0,0,0,0,0];
+function conf()
+{
+    gh[h]=gh[h]+1;
+    cancel();
+    start();
+}
+var hg;
+var arr=['CONGRESS','BJP','TDP','YSRCP','LOKSATTA','TRS','JSP','NOTA'];
+function displaywin()
+{  
+	var hy=0;
+	hg=window.open();
+	for(k=0;k<8;k++)
+	{
+		hg.document.write(arr[k]+"&nbsp;"+"="+"&nbsp;"+gh[k]+"</br>");
+		if(hy<gh[k])
+		{
+			hy=gh[k];
+		}
+	}
+	for(k=0;k<8;k++)
+	{
+		if(gh[k]==hy)
+		{
+			hg.document.write("</br></br></br></br>"+arr[k]+"&nbsp;<b>WON<b></br>");
+		}
+	}
+	
+}
+function closewin()
+{
+	hg.close();
+}
+
+
+		
+
+
+
